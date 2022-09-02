f8b3db9f662bfab032f3bd2392b9f79526bb6645
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-09-02 13:58:47 +0100

    Pricing logic ready to be implemented

--- backend/controllers/quotes.js
+++ backend/controllers/quotes.js
@@ -50,3 +50,50 @@ export const getQuoteByQuery = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+// Create the quote
+function getQuote(pet) {
+    // constants
+    const PRICE_MONTH = 10;
+    const DISCOUNTED_BREEDS = [
+        'Akita',
+        'Bull Terrier',
+        'Pug'
+    ];
+    const POSTCODE_INCREASE = [
+        'SW20',
+        'GU12',
+        'HU33'
+    ]
+
+    // separate the number of years and the leftover number of months
+    let monthsOld = pet.age % 12;
+    let yearsOld = pet.age > 11 ? (pet.age - monthsOld) / 12 : 0;
+
+    // init the quote with base price
+    let quote = PRICE_MONTH * pet.age;
+
+    // increase per year 5% 1-5 and 10% 6-10
+    if (pet.age > 11) {
+        
+        const discTen = yearsOld > 5 && yearsOld < 11 
+            ? yearsOld - 5
+            : yearsOld > 10 ? 5 : 0;
+        const discFive = yearsOld < 5 ? yearsOld : 5;
+
+        quote += +(((5 / 100) * (PRICE_MONTH * 12)) * discFive).toFixed(2);
+        quote += +(((10 / 100) * (PRICE_MONTH * 12)) * discTen).toFixed(2);
+    }
+
+    // check for area price increase
+    if (POSTCODE_INCREASE.includes(pet.city)) {
+        quote += +(15 / 100 * quote).toFixed(2);
+    }
+
+    // discount for pet breed
+    if (DISCOUNTED_BREEDS.includes(pet.breed)) {
+        quote -= +(10 / 100 * quote).toFixed(2);
+    }
+    
+    return quote;
+  }
